bdfe63ee99f74bf8a3f8c803fc415bacf128ba93
sunfeiyang <sfy5941@163.com>
2019-06-07 22:10:43 +0800

    项目打包部署配置完成

---
+++ xhh-admin-api/src/main/java/org/xhh/admin/SpringBootStartXHHApplication.java
@@ -0,0 +1,15 @@
+package org.xhh.admin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ * 使用项目用服务器启动
+ */
+public class SpringBootStartXHHApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
+}
